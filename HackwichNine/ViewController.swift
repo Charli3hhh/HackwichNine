bc749b109ee364397dd378d006b139a019c45a7f
Charles Ombao <charlesb.ombao@gmail.com>
2020-04-14 16:35:38 -1000

    Completed HackwichNine and Problem set 1, Continue Problem Set 2

--- HackwichNine/ViewController.swift
+++ HackwichNine/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var myLabel: UILabel!
+   
+    @IBOutlet weak var inputTextDisplay: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.myLabel.text = ""
+        segmentedControl.selectedSegmentIndex = -1
     }
 
-
+    @IBAction func segmentedControlPressed(_ sender: Any) {
+       
+        switch segmentedControl.selectedSegmentIndex {
+            
+        case 0:
+            self.myLabel.text = "Index 0 selected on the Segmented Control"
+        case 1:
+            self.myLabel.text = "Index 1 Selected, I got this"
+            
+        case 2:
+            self.myLabel.text = "Woohoo, this makes sense now"
+            
+        default:
+            break
+    
+        }
+    }
+    
 }
 
